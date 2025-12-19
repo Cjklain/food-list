74d8f863957aff4ee642d89d91e17e2a88f1c5eb
CJ <karolmmm01@gmail.com>
2025-12-19 12:44:49 +0100

    Start styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-sky-950">Start</h1>
+      <div className="h-screen">
+        <Header />
+        <div className="h-8/10 bg-yellow-50 flex justify-center">
+          <div className="bg-slate-600 w-1/10"></div>
+          <div className="bg-[url(./assets/lod.webp)] w-8/10 bg-contain bg-no-repeat"></div>
+          <div className="bg-slate-600 w-1/10"></div>
+        </div>
+        <div className="min-h-15 h-1/10 bg-indigo-300"></div>
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="min-h-15 h-1/10 border-b text-center d-flex bg-amber-100">
+      <h1 className="text-3xl font-bold underline text-sky-950">Start</h1>
+    </div>
+  );
+};
+
+export default Header;
